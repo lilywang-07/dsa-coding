b9ff89931cb5eacdde8eded0a30f296f8bd410c8
lilywang-07 <lilygwang07@gmail.com>
2026-09-04 23:07:27 -0500

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        n = len(s)
+        m = len(t)
+        if n < m:
+            return ''
+        remaining = m
+        curr = defaultdict(int)
+        length = float('inf')
+        start = 0
+        left = 0
+
+        for char in t:
+            curr[char] += 1
+
+        for right in range(n):
+            if s[right] in curr: # check if valid letter
+                if curr[s[right]] > 0:
+                    remaining -= 1
+                curr[s[right]] -= 1
+            while remaining == 0 and left <= right:
+                if right - left + 1 < length: # check length
+                    start = left
+                    length = right - left + 1
+                if s[left] in curr: # edits if start letter is necessary
+                    curr[s[left]] += 1
+                    if curr[s[left]] > 0:
+                        remaining += 1
+                left += 1
+
+        if length == float('inf'):
+            return ''
+        else:
+            return s[start:start + length]
